9abf8c6d4ba0a018a74142ba032bf98f5d4feda9
Treasure <treasureporth@MacBook-Pro-5.local>
2017-10-22 07:33:34 -0700

    Added cat list and cat detail view

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 (function(){
+
     //Create kittens 
     function Kitten(name, imgUrl){
         this.name = name;
@@ -10,12 +11,15 @@
     const kittens = [
         new Kitten("Barty"), 
         new Kitten("Greta", "cute-kitten2.jpg"), 
-        new Kitten("Sam", "cute-kitten3.jpg")
+        new Kitten("Sam", "cute-kitten3.jpg"),
+        new Kitten("Marge", "cute-kitten4.jpg"),
+        new Kitten("Lester", "cute-kitten5.jpg")
     ];
 
     //display kittens
     const kittenTemplate = kittens.map(function(kitten){
         const kittyID = kitten.name.toLowerCase();
+ 
         const kitty = `
         <div class="kitten-container" id="${kittyID}">
             <h2>${kitten.name}</h2>
@@ -26,20 +30,42 @@
         return kitty
     });
 
-    $('.container').append(kittenTemplate);
+    const catList = kittens.map(function(kitten){
+        return `<li>${kitten.name}</li>`
+    })
+
+     //page setup
+
+     $('.cat-list').append(catList);
+     $('.container').append(kittenTemplate);
+     $('.cat-detail').append(kittenTemplate[0]);
 
     //count kittens
     function incrementCounter(kitten){
         kitten.counter = kitten.counter + 1;
     }
+
     $('.container').on("click", ".kitten-container img", function(event){
         const currentKitten = $(this).parent().attr("id");
         
         kittens.forEach(function(kitten){
             if (kitten.name.toLowerCase() === currentKitten){
                 incrementCounter(kitten);
-                $(event.target).next().html(`${kitten.name} clicked ${kitten.counter} times.`);
+                $(event.target).parent().find('div').html(`${kitten.name} clicked ${kitten.counter} times.`);
             }
         });
     });
+
+    $('.container').on("click", ".cat-list li", function(event){
+        const currentKitten = $(this).html().toLowerCase();
+
+        kittenTemplate.forEach(function(kitten){
+            if (currentKitten === $(kitten).attr('id')){
+                $('.cat-detail').empty();
+                $('.cat-detail').append(kitten);
+            }    
+        });
+    });
 })();
+
+//TODO: Fix bug where counter text resets to 0 when switching between kittens 
